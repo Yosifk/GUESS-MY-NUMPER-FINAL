228e1a758cec654014fa362209bb7c6898641ed1
Yosifk <121035067+Yosifk@users.noreply.github.com>
2023-09-01 16:36:55 +0300

    Update script.js
    its the same code with less lines

--- script.js
+++ script.js
@@ -8,15 +8,18 @@ document.querySelector('.guess').value =23*/
 let Snumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let hightScorre =0;
+ const disblayMessage = function (message) {
+  document.querySelector('.message').textContent =message;
+ }
 document.querySelector('.check').addEventListener('click', function () {
   let guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
   //when there is no input
   if (!guess) {
-    document.querySelector('.message').textContent = '🚫 no numper 🚫';
+      disblayMessage('🚫 no numper 🚫');
     //when player wins the game
   } else if (guess === Snumber) {
-    document.querySelector('.message').textContent = 'correct numper🎉';
+    disblayMessage( 'correct numper🎉');
     document.querySelector('body').style.backgroundColor='#60b347';
     document.querySelector('.number').style.width='30rem';
     document.querySelector('.number').textContent = Snumber;
@@ -24,46 +27,26 @@ document.querySelector('.check').addEventListener('click', function () {
         hightScorre=score;
         document.querySelector('.highscore').textContent=hightScorre;
     }
-    else if (guess!==Snumber) {
+  }
+    else if (guess !== Snumber) {
         if (score > 1) {
-            document.querySelector('.message').textContent =guess>Snumber? 'Too hight ⬆️':'Too low ⬇️';
+            disblayMessage(guess > Snumber ? 'Too hight ⬆️':'Too low ⬇️');
             score--;
             document.querySelector('.score').textContent = score;
           } else {
-            document.querySelector('.message').textContent = 'Loser 👎';
+            disblayMessage('Loser 👎');
             document.querySelector('.score').textContent = 0;
           }
     }
-
-    //when guess is too hight 
-  } else if (guess > Snumber) {
-    if (score > 1) {
-      document.querySelector('.message').textContent = 'Too hight ⬆️';
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = 'Loser 👎';
-      document.querySelector('.score').textContent = 0;
-    }
-    //when guess is too low 
-  } else if (guess < Snumber) {
-    if (score > 1) {
-      document.querySelector('.message').textContent = 'Too low ⬇️';
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = 'Loser 👎';
-      document.querySelector('.score').textContent = 0;
-    }
   }
-});
+);
 document.querySelector('.again').addEventListener('click', function () {
     document.querySelector('body').style.backgroundColor='#222';
     document.querySelector('.number').style.width='15rem';
     document.querySelector('.score').textContent = 20;
     Snumber = Math.trunc(Math.random() * 20) + 1;
     document.querySelector('.number').textContent = "?";
-    document.querySelector('.message').textContent = 'Start guessing ....';
+    disblayMessage( 'Start guessing ....');
     document.querySelector('.guess').value="";
 
 
